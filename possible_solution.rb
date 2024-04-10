bade7350983186d7e93d16bf407bfd14fef8151f
austinj33 <164961440+austinj33@users.noreply.github.com>
2024-04-10 00:11:47 +0000

    update

--- possible_solution.rb
+++ possible_solution.rb
@@ -1,3 +1,5 @@
+
+
 require "http"
 require "json"
 
---
+++ umbrella.rb
@@ -0,0 +1,10 @@
+pp "howdy"
+
+require "http"
+require "json"
+
+pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER")
+
+
+gmaps_key = ENV.fetch("GMAPS")
+
