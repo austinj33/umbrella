7194737fd8e6044aa317a8a13da6006ba2766062
austinj33 <164961440+austinj33@users.noreply.github.com>
2024-04-15 00:22:43 +0000

    finished

--- umbrella.rb
+++ umbrella.rb
@@ -8,3 +8,14 @@ pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER")
 
 gmaps_key = ENV.fetch("GMAPS")
 
+pp "Where are you located?"
+
+# user_location = gets.chomp
+
+user_location = "Chicago"
+
+maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address="+ user_location +"key=" + ENV.fetch("GMAPS")
+
+resp = HTTP.get(maps_url)
+
+pp resp.to_s
